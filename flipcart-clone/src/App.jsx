262696dec850bc61a68e410d1c9786b4f51ff03a
archDjango <pranavborude03@gmail.com>
2025-02-26 14:44:45 +0530

    Update App.jsx

--- flipcart-clone/src/App.jsx
+++ flipcart-clone/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
       <Navbar />
-      {/* Rest of your application */}
+      <main className="main-content">
+        {/* Your main content goes here */}
+        <h1>Welcome to Flipkart Clone</h1>
+        <p>This is the main content area.</p>
+      </main>
+      <Footer />
     </div>
   );
 }
